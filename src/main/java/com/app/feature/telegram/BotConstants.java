af27c6ed77cbab99b274020078005e80ec39862a
Stas Kotelnykov <staskotelnykov@gmail.com>
2022-09-23 01:03:35 +0300

    token changed

--- src/main/java/com/app/feature/telegram/BotConstants.java
+++ src/main/java/com/app/feature/telegram/BotConstants.java
@@ -2,5 +2,5 @@ package com.app.feature.telegram;
 
 public class BotConstants {
     public static final String BOT_NAME = "CurrencyExchangeChatBot";
-    public static final String BOT_TOKEN = "5678676513:AAGpT6LVLIxPil_RZ8u5FTeqKTln0dFvxDo";
+    public static final String BOT_TOKEN = "5678676513:AAHwvszZCSKy7vmB3vFH8apsuuJNRShv54M";
 }
