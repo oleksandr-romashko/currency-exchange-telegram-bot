6da6f959145967dbd2dae602b008fc5d24eb5ce5
Stas Kotelnykov <staskotelnykov@gmail.com>
2022-09-26 01:08:55 +0300

    added submenu buttons

--- src/main/java/com/app/feature/telegram/CurrencyTelegramBot.java
+++ src/main/java/com/app/feature/telegram/CurrencyTelegramBot.java
@@ -33,7 +33,7 @@ public class CurrencyTelegramBot extends TelegramLongPollingCommandBot {
         SendMessage message = new SendMessage();
         message.setText(text);
         message.setChatId(chatId);
-        if(keyboard != null) {
+        if (keyboard != null) {
             message.setReplyMarkup(keyboard);
         }
 
@@ -110,29 +110,127 @@ public class CurrencyTelegramBot extends TelegramLongPollingCommandBot {
     private void onDecimalPlacesPressed() {
         System.out.println("Decimal_places pressed!");
 
-        String text = "Кілкість знаків після коми";
-        sendMessageWithKeyboard(text, chatId, null);
+        String text = "Select the number of decimal places:";
+        List<InlineKeyboardButton> keyboardRow1 = new ArrayList<>();
+        List<InlineKeyboardButton> keyboardRow2 = new ArrayList<>();
+        List<InlineKeyboardButton> keyboardRow3 = new ArrayList<>();
+        InlineKeyboardButton twoDecimalPlacesButton = InlineKeyboardButton.builder().text("2").callbackData("two_decimal_places").build();
+        InlineKeyboardButton threeDecimalPlacesButton = InlineKeyboardButton.builder().text("3").callbackData("three_decimal_places").build();
+        InlineKeyboardButton fourDecimalPlacesButton = InlineKeyboardButton.builder().text("4").callbackData("four_decimal_places").build();
+        keyboardRow1.add(twoDecimalPlacesButton);
+        keyboardRow2.add(threeDecimalPlacesButton);
+        keyboardRow3.add(fourDecimalPlacesButton);
+
+        List<List<InlineKeyboardButton>> rowList = new ArrayList<>();
+        rowList.add(keyboardRow1);
+        rowList.add(keyboardRow2);
+        rowList.add(keyboardRow3);
+
+        InlineKeyboardMarkup keyboard = InlineKeyboardMarkup
+                .builder()
+                .keyboard(rowList)
+                .build();
+
+        sendMessageWithKeyboard(text, chatId, keyboard);
     }
 
     private void onCurrencyPressed() {
         System.out.println("Currency pressed!");
 
-        String text = "Вибір валюти";
-        sendMessageWithKeyboard(text, chatId, null);
+        String text = "Select the currency/currencies:";
+        List<InlineKeyboardButton> keyboardRow1 = new ArrayList<>();
+        List<InlineKeyboardButton> keyboardRow2 = new ArrayList<>();
+        List<InlineKeyboardButton> keyboardRow3 = new ArrayList<>();
+        InlineKeyboardButton usdButton = InlineKeyboardButton.builder().text("USD").callbackData("USD").build();
+        InlineKeyboardButton eurButton = InlineKeyboardButton.builder().text("EUR").callbackData("EUR").build();
+        InlineKeyboardButton rubButton = InlineKeyboardButton.builder().text("RUB").callbackData("RUB").build();
+        keyboardRow1.add(usdButton);
+        keyboardRow2.add(eurButton);
+        keyboardRow3.add(rubButton);
+
+        List<List<InlineKeyboardButton>> rowList = new ArrayList<>();
+        rowList.add(keyboardRow1);
+        rowList.add(keyboardRow2);
+        rowList.add(keyboardRow3);
+
+        InlineKeyboardMarkup keyboard = InlineKeyboardMarkup
+                .builder()
+                .keyboard(rowList)
+                .build();
+
+        sendMessageWithKeyboard(text, chatId, keyboard);
     }
 
     private void onNotificationTimePressed() {
         System.out.println("Notification_time pressed!");
 
-        String text = "Час сповіщень";
-        sendMessageWithKeyboard(text, chatId, null);
+        String text = "Select notification time:";
+        List<InlineKeyboardButton> keyboardRow1 = new ArrayList<>();
+        List<InlineKeyboardButton> keyboardRow2 = new ArrayList<>();
+        List<InlineKeyboardButton> keyboardRow3 = new ArrayList<>();
+        List<InlineKeyboardButton> keyboardRow4 = new ArrayList<>();
+        InlineKeyboardButton nineButton = InlineKeyboardButton.builder().text("9").callbackData("9").build();
+        InlineKeyboardButton tenButton = InlineKeyboardButton.builder().text("10").callbackData("10").build();
+        InlineKeyboardButton elevenButton = InlineKeyboardButton.builder().text("11").callbackData("11").build();
+        InlineKeyboardButton twelveButton = InlineKeyboardButton.builder().text("12").callbackData("12").build();
+        InlineKeyboardButton thirteenButton = InlineKeyboardButton.builder().text("13").callbackData("13").build();
+        InlineKeyboardButton fourteenButton = InlineKeyboardButton.builder().text("14").callbackData("14").build();
+        InlineKeyboardButton fifteenButton = InlineKeyboardButton.builder().text("15").callbackData("15").build();
+        InlineKeyboardButton sixteenButton = InlineKeyboardButton.builder().text("16").callbackData("16").build();
+        InlineKeyboardButton seventeenButton = InlineKeyboardButton.builder().text("17").callbackData("17").build();
+        InlineKeyboardButton eighteenButton = InlineKeyboardButton.builder().text("18").callbackData("18").build();
+        InlineKeyboardButton turnOffNotificationsButton = InlineKeyboardButton.builder().text("Turn off notifications").callbackData("turn_off_notifications").build();
+        keyboardRow1.add(nineButton);
+        keyboardRow1.add(tenButton);
+        keyboardRow1.add(elevenButton);
+        keyboardRow2.add(twelveButton);
+        keyboardRow2.add(thirteenButton);
+        keyboardRow2.add(fourteenButton);
+        keyboardRow3.add(fifteenButton);
+        keyboardRow3.add(sixteenButton);
+        keyboardRow3.add(seventeenButton);
+        keyboardRow4.add(eighteenButton);
+        keyboardRow4.add(turnOffNotificationsButton);
+
+        List<List<InlineKeyboardButton>> rowList = new ArrayList<>();
+        rowList.add(keyboardRow1);
+        rowList.add(keyboardRow2);
+        rowList.add(keyboardRow3);
+        rowList.add(keyboardRow4);
+
+        InlineKeyboardMarkup keyboard = InlineKeyboardMarkup
+                .builder()
+                .keyboard(rowList)
+                .build();
+
+        sendMessageWithKeyboard(text, chatId, keyboard);
     }
 
     private void onBankPressed() {
         System.out.println("Bank pressed!");
 
-        String text = "Обрати банк";
-        sendMessageWithKeyboard(text, chatId, null);
+        String text = "Select the bank/banks:";
+        List<InlineKeyboardButton> keyboardRow1 = new ArrayList<>();
+        List<InlineKeyboardButton> keyboardRow2 = new ArrayList<>();
+        List<InlineKeyboardButton> keyboardRow3 = new ArrayList<>();
+        InlineKeyboardButton nbuButton = InlineKeyboardButton.builder().text("NBU").callbackData("NBU").build();
+        InlineKeyboardButton privatBankButton = InlineKeyboardButton.builder().text("PrivatBank").callbackData("PrivatBank").build();
+        InlineKeyboardButton monoBankButton = InlineKeyboardButton.builder().text("MonoBank").callbackData("MonoBank").build();
+        keyboardRow1.add(nbuButton);
+        keyboardRow2.add(privatBankButton);
+        keyboardRow3.add(monoBankButton);
+
+        List<List<InlineKeyboardButton>> rowList = new ArrayList<>();
+        rowList.add(keyboardRow1);
+        rowList.add(keyboardRow2);
+        rowList.add(keyboardRow3);
+
+        InlineKeyboardMarkup keyboard = InlineKeyboardMarkup
+                .builder()
+                .keyboard(rowList)
+                .build();
+
+        sendMessageWithKeyboard(text, chatId, keyboard);
     }
 
     @Override
