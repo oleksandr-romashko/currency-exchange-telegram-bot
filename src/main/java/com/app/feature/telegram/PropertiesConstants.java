2a4aec4f4936ee6613cce3478c44e39f417eb4ef
Oleksandr Romashko <alex.rmshk@gmail.com>
2022-09-29 07:29:27 +0200

    Refactor PropertiesConstants

--- src/main/java/com/app/feature/telegram/PropertiesConstants.java
+++ src/main/java/com/app/feature/telegram/PropertiesConstants.java
@@ -5,19 +5,17 @@ import java.io.IOException;
 import java.util.Properties;
 
 public class PropertiesConstants {
+    private static final String propertiesPath = "./src/main/resources/application.properties";
     public String propertiesReader(String propertyName) {
         Properties property = new Properties();
-
         String propertyValue = null;
         try {
-            FileInputStream inputStream = new FileInputStream("./src/main/resources/application.properties");
+            FileInputStream inputStream = new FileInputStream(propertiesPath);
             property.load(inputStream);
-
             propertyValue = property.getProperty(propertyName);
         } catch (IOException e) {
             System.err.println("ERROR: properties file does not exist!");
         }
         return propertyValue;
     }
-
 }
