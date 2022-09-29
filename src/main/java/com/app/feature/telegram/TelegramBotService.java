5efdd476f2b43a7ddf3a640c55e65800bc929cdf
Oleksandr Romashko <alex.rmshk@gmail.com>
2022-09-29 07:27:13 +0200

    Refactor TelegramBotService

--- src/main/java/com/app/feature/telegram/TelegramBotService.java
+++ src/main/java/com/app/feature/telegram/TelegramBotService.java
@@ -5,14 +5,12 @@ import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
 
 public class TelegramBotService {
-    private CurrencyTelegramBot currencyTelegramBot;
 
     public TelegramBotService() {
-        currencyTelegramBot = new CurrencyTelegramBot();
+        CurrencyTelegramBot currencyTelegramBot = new CurrencyTelegramBot();
         try {
             TelegramBotsApi telegramBotsApi = new TelegramBotsApi(DefaultBotSession.class);
             telegramBotsApi.registerBot(currencyTelegramBot);
-
         } catch (TelegramApiException e) {
             e.printStackTrace();
         }
