bed2093e94103b6cd5e4bfce25a238b825762e9d
Oleksandr Romashko <alex.rmshk@gmail.com>
2022-09-29 14:04:59 +0200

    Update PrettyPrint to new api

--- src/main/java/com/app/feature/telegram/ui/PrettyPrintCurrencyService.java
+++ src/main/java/com/app/feature/telegram/ui/PrettyPrintCurrencyService.java
@@ -1,6 +1,6 @@
 package com.app.feature.telegram.ui;
 
-import com.app.feature.currency.dto.CurrencyItem;
+import com.app.feature.api.dto.AbstractCurrencyItem;
 import com.vdurmont.emoji.EmojiParser;
 
 import java.util.List;
@@ -8,24 +8,23 @@ import java.util.List;
 public class PrettyPrintCurrencyService {
     private final String euro_emoji = EmojiParser.parseToUnicode(":eu:");
     private final String dollar_emoji = EmojiParser.parseToUnicode(":us:");
-    private final String bitcoin_emoji = EmojiParser.parseToUnicode(":moneybag:");
     private final String rub_emoji = EmojiParser.parseToUnicode(":ru:");
     private final String ua_emoji = EmojiParser.parseToUnicode(":ua:");
 
-    public String convert(List<CurrencyItem> currencyItemPrivatList, int rounding) {
+    public String convert(List<AbstractCurrencyItem> currencyItemPrivatList, int rounding) {
         StringBuilder res = new StringBuilder("Exchange rate in PrivatBank:");
-        for(CurrencyItem currencyItemPrivat : currencyItemPrivatList) {
-            String currencyFirstFlag = getCurrencyFlag(currencyItemPrivat.getCcy().name());
-            String currencySecondFlag = getCurrencyFlag(currencyItemPrivat.getBase_ccy().name());
+        for(AbstractCurrencyItem currencyItemPrivat : currencyItemPrivatList) {
+            String currencyFirstFlag = getCurrencyFlag(currencyItemPrivat.getCurrency().name());
+            String currencySecondFlag = getCurrencyFlag(currencyItemPrivat.getBaseCurrency().name());
 
             float roundedBuyRate = Math.round(currencyItemPrivat.getBuy() * Math.pow(10, rounding))/(float)Math.pow(10, rounding);
             float roundedSaleRate = Math.round(currencyItemPrivat.getSale() * Math.pow(10, rounding))/(float)Math.pow(10, rounding);
 
             res.append(System.lineSeparator())
                     .append(" ".repeat(4))
-                    .append(currencyItemPrivat.getBase_ccy())
+                    .append(currencyItemPrivat.getBaseCurrency())
                     .append("/")
-                    .append(currencyItemPrivat.getCcy().name())
+                    .append(currencyItemPrivat.getCurrency().name())
                     .append(" ".repeat(3))
                     .append(currencySecondFlag)
                     .append("/")
@@ -41,13 +40,12 @@ public class PrettyPrintCurrencyService {
         return res.toString();
     }
 
-    public String getCurrencyFlag(String currencyName) {
+    private String getCurrencyFlag(String currencyName) {
         return switch (currencyName) {
-            case "UAH" -> ua_emoji;
-            case "BTC" -> bitcoin_emoji;
             case "EUR" -> euro_emoji;
             case "USD" -> dollar_emoji;
             case "RUB" -> rub_emoji;
+            case "UAH" -> ua_emoji;
             default -> null;
         };
     }
