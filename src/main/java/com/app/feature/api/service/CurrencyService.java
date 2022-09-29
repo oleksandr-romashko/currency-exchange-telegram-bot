5bee8081554c018880d5d725b366336a4bc5b0c2
Oleksandr Romashko <alex.rmshk@gmail.com>
2022-09-29 07:58:17 +0200

    Refactor CurrencyService

--- src/main/java/com/app/feature/api/service/CurrencyService.java
+++ src/main/java/com/app/feature/api/service/CurrencyService.java
@@ -2,10 +2,9 @@ package com.app.feature.api.service;
 
 import com.app.feature.api.dto.Currency;
 
-import java.io.IOException;
 import java.util.Map;
 
 public interface CurrencyService {
-    Map<String, Double> getRate(Currency currency) throws IOException;
+    Map<String, Double> getRate(Currency currency);
 }
 
