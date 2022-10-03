de3e9fc77650812fdd7c381ea99e1e605285739c
Oleksandr Romashko <alex.rmshk@gmail.com>
2022-10-03 19:22:48 +0200

    Update NBUCurrency service

--- src/main/java/com/app/feature/api/service/NBUCurrencyService.java
+++ src/main/java/com/app/feature/api/service/NBUCurrencyService.java
@@ -1,51 +1,94 @@
 package com.app.feature.api.service;
 
-import com.app.feature.api.CurrencyJsonUpdate;
-import com.app.feature.api.Utilities;
+import com.app.feature.api.CurrencyFileReaderService;
+import com.app.feature.api.dto.AbstractCurrencyItem;
 import com.app.feature.api.dto.Currency;
 import com.google.gson.Gson;
 import com.google.gson.reflect.TypeToken;
-import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
 
 import java.lang.reflect.Type;
-import java.util.HashMap;
+import java.util.ArrayList;
 import java.util.List;
-import java.util.Map;
-import java.util.stream.Collectors;
 
 public class NBUCurrencyService implements CurrencyService {
 
     @Override
-    public Map<String, Double> getRate(Currency currency) {
+    public List<AbstractCurrencyItem> getRates(List<Currency> savedCurrency) {
 
-        //take json from file
-        String takeJsonFromFile = Utilities.writeFromJsonFile(CurrencyJsonUpdate.getABSOLUTE_PATH_NBU());
+        //get JSON from file
+        String json = new CurrencyFileReaderService().getRatesForNbu();
 
-        //Convert json => Java Object
+        //convert JSON to Java objects
         Type typeToken = TypeToken
                 .getParameterized(List.class, CurrencyItemNBU.class)
                 .getType();
-        List<CurrencyItemNBU> currencyItemsNBU = new Gson().fromJson(takeJsonFromFile, typeToken);
+        List<CurrencyItemNBU> receivedApiCurrencies = new Gson()
+                .fromJson(json, typeToken);
 
-        //Find currency
-        double currencyRate = currencyItemsNBU.stream()
-                .filter(it -> it.getCc() == currency)
-                .map(CurrencyItemNBU::getRate)
-                .collect(Collectors.toList()).get(0);
-
-        Map<String, Double> rate = new HashMap<>();
-        rate.put("rate" + currency, currencyRate);
-
-        return rate;
+        //find CurrencyItem
+        List<AbstractCurrencyItem> resultList = new ArrayList<>();
+        for(Currency currency : savedCurrency) {
+            AbstractCurrencyItem currencyItem = receivedApiCurrencies
+                    .stream()
+                    .filter(it -> it.getCurrency().equals(currency.name()))
+                    .findFirst()
+                    .orElseThrow();
+            resultList.add(currencyItem);
+        }
+        return resultList;
     }
 
-    @Data
-    public static class CurrencyItemNBU {
+    /*
+     * Example of reply JSON:
+     * [
+     *   {},
+     *   ...
+     *   ,{
+     *     "r030":978,
+     *     "txt":"Євро",
+     *     "rate":34.9742,
+     *     "cc":"EUR",
+     *     "exchangedate":"29.09.2022"
+     *   },
+     *   ...,
+     *   {}
+     * ]
+     */
+    public static class CurrencyItemNBU extends AbstractCurrencyItem {
+        @Getter
+        @Setter
         private int r030;
+        @Getter
+        @Setter
         private String txt;
+        @Setter
         private float rate;
-        private Currency cc;
-        private String exchangeDate;
-    }
+        @Setter
+        private String cc;
+        @Getter
+        @Setter
+        private String exchangedate;
 
+        @Override
+        public String getCurrency() {
+            return cc;
+        }
+
+        @Override
+        public float getBuy() {
+            return rate;
+        }
+
+        @Override
+        public float getSale() {
+            return 0;
+        }
+
+        @Override
+        public Currency getBaseCurrency() {
+            return Currency.UAH;
+        }
+    }
 }
